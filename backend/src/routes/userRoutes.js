835c0d6bb32846c5b14a04ffa6ad7fa16b725641
Malayali Developer <121072697+MalayaliDev@users.noreply.github.com>
2025-12-01 11:47:33 +0530

    Update userRoutes.js

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -4,6 +4,10 @@ const userController = require('../controllers/userController');
 
 const router = express.Router();
 
+// Admin endpoints
+router.get('/all', auth(['staff', 'admin']), userController.getAllUsers);
+
+// User endpoints
 router.get('/phone', auth(['student', 'staff', 'admin']), userController.getPhoneNumber);
 router.post('/phone', auth(['student', 'staff', 'admin']), userController.savePhoneNumber);
 router.get('/webhook', auth(['student', 'staff', 'admin']), userController.getDiscordWebhook);
